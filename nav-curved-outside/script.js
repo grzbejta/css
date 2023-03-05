bc6d95fbd936a397283f551a87506598371d51cf
reddeusdev <red.deus.dev@gmail.com>
2023-03-05 14:14:35 +0100

    up nav curved
     add  rwd

--- nav-curved-outside/script.js
+++ nav-curved-outside/script.js
@@ -1,4 +1,6 @@
-const list = document.querySelectorAll(".navigation__list");
+const list = document.querySelectorAll(".navigation__list"),
+  menu = document.querySelector(".navigation__lists"),
+  opernMenu = document.querySelector(".nav__toggle");
 function active() {
   list.forEach((item) => {
     item.classList.remove("active");
@@ -7,4 +9,15 @@ function active() {
 }
 list.forEach((item) => {
   item.addEventListener("click", active);
+});
+
+// close-circle-outline
+let flaga = true;
+opernMenu.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  opernMenu.setAttribute(
+    "name",
+    `${flaga ? "close-circle-outline" : "menu-outline"}`
+  );
+  flaga = !flaga;
 });
